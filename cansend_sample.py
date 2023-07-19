2ad126087c3458c2937f77dfccefeb60a590dbe6
Jim Marquardson <jim.marquardson@gmail.com>
2023-07-19 11:00:40 -0400

    Update cansend_sample.py
    Added CC0 license

--- cansend_sample.py
+++ cansend_sample.py
@@ -1,4 +1,10 @@
-# This is a simple python progrm that runs cansend  on 
+# Created by Dr. Jim Marquardson
+# Initial commit: 7/18/2023
+# Last updated: 7/19/2023
+# License: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication (https://creativecommons.org/publicdomain/zero/1.0/)
+# Use, modify, and distribute this code as you see fit.
+#
+# This is a simple python program that runs cansend  on 
 # the vcan0 network. It sends a CAN message with the
 # arbitration ID 244 and data 0000005A12 10 times per second.
 from subprocess import call
